9e04087db524f5547478f22d10512fd35170ee26
Moises Munaldi <ork.mgm@gmail.com>
2024-02-14 13:33:42 +0000

    homepage done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
-import { Carousel } from './layouts/NavbarAndFooter/HomePage/Carousel';
+import { Navbar } from './layouts/NavbarAndFooter/NavbarAndFooter/Navbar';
+import { Footer } from './layouts/NavbarAndFooter/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/NavbarAndFooter/HomePage/HomePage';
 
-function App() {
+export const App = () => {
   return (
     <div>
-    <Navbar />
-    <ExploreTopBooks/>
-    <Carousel/>
+      <Navbar />
+      <HomePage />
+      <Footer />
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/layouts/NavbarAndFooter/HomePage/components/Heros.tsx
@@ -0,0 +1,73 @@
+export const Heros = () => {
+  return (
+    <div>
+      <div className="d-none d-lg-block">
+        <div className="row g-0 mt-5">
+          <div className="col-sm-6 col-md-6">
+            <div className="col-image-left"></div>
+          </div>
+          <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+            <div className="ml-2">
+              <h1>What have you been reading</h1>
+              <p className="lead">
+                The library team would love to know what you have bene reading.
+                blablablalbalblablalbalblabalbalblablalbal we will provide to
+                pcontant for you
+              </p>
+              <a className="btn main-color btn-lg text-white" href="#">
+                Sign up
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="row g-0">
+          <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+            <div className="ml-2">
+              <h1>Our colection balblablabl</h1>
+              <p className="lead">
+                Try to check out in daily balblablalba We eork nonstop to select
+                lblablablalbalblalblbablaablbalbal
+                balblablablablbalablablbalabcacsjicscacsacasicsaicsaiisacs
+                csajajsccsajcajscjijcasacjsjacscsjajcs
+              </p>
+            </div>
+          </div>
+          <div className="col-sm-6 col-md-6">
+            <div className="col-image-right"></div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Heros */}
+      <div className="d-lg-none">
+        <div className="container">
+          <div className="m-2">
+            <div className="col-image-left"></div>
+            <div className="mt-2">
+              <h1>What have you been reading?</h1>
+              <p className="lead">
+                The library team would love to know what you have been reading.
+                Whether it is to learn a new skill or grow within one, we will
+                be able to provide the top content for you!
+              </p>
+              <a className="btn main-color btn-lg text-white" href="#">
+                Sign up
+              </a>
+            </div>
+          </div>
+          <div className="m-2">
+            <div className="col-image-right"></div>
+            <div className="mt-2">
+              <h1>Our colection balblablabl</h1>
+              <p className="lead">
+                Try to check out in daily balblablalba We eork nonstop to select
+                lblablablalbalblalblbablaablbalbal balblablablablbalablablbalabc
+                csajajsccsajcajscjijcasacjsjacscsjajcs
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
