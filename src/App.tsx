552c45ab81c9d9f13db29d1b87a2108c9c6c6b0c
Moises Munaldi <ork.mgm@gmail.com>
2024-02-21 21:52:08 +0000

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import { Navbar } from './layouts/NavbarAndFooter/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/NavbarAndFooter/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Route } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      <Route path="/">
+        <HomePage />
+      </Route>
+      <Route path="/search">
+        <SearchBooksPage />
+      </Route>
       <Footer />
     </div>
   );
